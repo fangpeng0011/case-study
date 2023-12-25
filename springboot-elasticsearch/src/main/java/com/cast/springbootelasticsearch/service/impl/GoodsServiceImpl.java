65c1177831deedfff5a257820d4e4a765641b9d6
fangpeng0011 <fangpeng0011@163.com>
2023-12-25 20:30:49 +0800

    初始化

--- springboot-elasticsearch/src/main/java/com/cast/springbootelasticsearch/service/impl/GoodsServiceImpl.java
+++ springboot-elasticsearch/src/main/java/com/cast/springbootelasticsearch/service/impl/GoodsServiceImpl.java
@@ -35,9 +35,42 @@ public class GoodsServiceImpl implements GoodsService {
         goods.setPrice(4230.0);
         goods.setCreateTime(new Date());
         goods.setTmId(1L);
+        goods.setCategory1Id(1L);
         goods.setTmName("华为");
-        Goods save = repository.save(goods);
-        log.info(save.toString());
+
+        Goods goods1 = new Goods();
+        goods1.setId(2L);
+        goods1.setTitle("华为手机快人二步");
+        goods1.setPrice(4240.0);
+        goods1.setCreateTime(new Date());
+        goods1.setCategory1Id(2L);
+        goods1.setTmId(2L);
+        goods1.setTmName("华为");
+
+        Goods goods2 = new Goods();
+        goods2.setId(3L);
+        goods2.setTitle("苹果手机");
+        goods2.setPrice(9240.0);
+        goods2.setCreateTime(new Date());
+        goods2.setTmId(3L);
+        goods1.setCategory1Id(3L);
+        goods2.setTmName("苹果");
+
+        Goods goods3 = new Goods();
+        goods3.setId(4L);
+        goods3.setTitle("小米手机，屌丝专用");
+        goods3.setPrice(5240.0);
+        goods3.setCreateTime(new Date());
+        goods3.setTmId(4L);
+        goods1.setCategory1Id(4L);
+        goods3.setTmName("小米");
+
+        List<Goods> goodsList = new ArrayList<>();
+        goodsList.add(goods);
+        goodsList.add(goods1);
+        goodsList.add(goods2);
+        goodsList.add(goods3);
+        repository.saveAll(goodsList);
     }
 
 
